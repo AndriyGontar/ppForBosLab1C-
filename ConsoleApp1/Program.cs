12e62b7641a4022a09fcec6097cb1c78ca24aa9d
Andriy Gontar <148362936+AndriyGontar@users.noreply.github.com>
2024-09-17 21:30:57 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static int lenArray = 5000000;
+        private static int[] colsThread = new int[] { 1, 2, 3, 4, 8, 16, 32 };
+
+        public static void Main(String[] args)
+        {
+            testThreads();
+        }
+
+        private static void testThreads()
+        {
+            ThreadSum threadSum = new ThreadSum();
+            var arr = threadSum.GenerateArray(lenArray);
+            var startTime = DateTime.Now;
+            //long sum = arr.Sum();
+            var stopTime = DateTime.Now;
+            var time = stopTime- startTime;
+            //Console.WriteLine("Sum array calculated by built-in function is "
+            //        + sum + ". This continues " + time + " milliseconds");
+            foreach (int j in colsThread)
+            {
+                startTime = DateTime.Now; 
+                long sum = threadSum.GetSumArray(arr, j);
+                stopTime = DateTime.Now;
+                time = stopTime - startTime;
+                Console.WriteLine("Sum with " + j + " threads is " + sum
+                        + ". This continues " + time + " milliseconds");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ThreadSum.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    internal class ThreadSum
+    {
+        private long sum;
+        private readonly Object sumLock = new();
+        private int collEndThreads=0;
+        public ThreadSum()
+        {
+        }
+
+        public int[] GenerateArray(int lenArray)
+        {
+            Random random = new Random();
+            int[] array = new int[lenArray];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next();
+            }
+            return array;
+        }
+
+        private void AddSum(long sumElement)
+        {
+            lock (sumLock)
+            {
+                sum += sumElement;
+                collEndThreads++;
+            }
+
+        }
+
+        private void SumElement(int start, int stop, int[] array)
+        {
+            long sumElementAraay = 0;
+            for (int i = start; i < stop; i++)
+            {
+                sumElementAraay += array[i];
+            }
+            AddSum(sumElementAraay);
+        }
+
+        public long GetSumArray(int[] array, int colThread)
+        {
+            collEndThreads = 0;
+            sum = 0;
+            int len = array.Length / colThread;
+            for (int i = 0; i < colThread; i++)
+            {
+                int start = len * i;
+                int stop = i != (colThread - 1) ? len * (i + 1) : array.Length;
+                Thread thread = new Thread(() => SumElement(start, stop, array));
+                thread.Start();
+            }
+            while (collEndThreads < colThread)
+            {
+            }
+            return sum;
+        }
+    }
+}
